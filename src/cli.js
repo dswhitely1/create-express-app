a04e966221c0e5223cd80ff694afabd0bf77b18a
Donald Whitely <dswhitely1@gmail.com>
2020-05-23 17:57:10 -0400

    cli and create-express-app defined

---
+++ src/cli.js
@@ -0,0 +1,45 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+async function getConfig(rawArgs) {
+    const args = arg({
+        '--typescript': Boolean,
+        '-ts': '--typescript'
+    }, {argv: rawArgs.slice(2)})
+
+    const options = {
+        directory: args._[0],
+        typescript: args['--typescript'] || false
+    }
+
+    const questions = []
+
+    if (!options.directory) {
+        questions.push({
+            name: 'directory',
+            message: 'Please name your project',
+            default: 'api'
+        })
+    }
+
+    if (!options.typescript) {
+        questions.push({
+            name: 'typescript',
+            message: 'Would you like to use typescript for this project?'
+            type: 'confirm',
+            default: false
+        })
+    }
+
+    const answers = await inquirer(questions);
+
+    return {
+        directory: options.directory || answers.directory,
+        typescript: options.typescript || answers.typescript
+    }
+}
+
+export async function cli(args) {
+    const options = await getConfig(args);
+    console.log(options);
+}
